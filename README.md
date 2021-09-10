da98520f1dbedc24a8f4aee986347c1eaf687fa3
Jessica Kopps <jessicakopps@gmail.com>
2021-09-09 22:45:19 -0300

    Domain model, database seed

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 Projeto desenvolvido na Semana Spring React do DevSuperior com o professor Nelio Alves.
 
+### Tecnologias usadas
+
+Spring Boot com BD relacional, entrega contínua
+
+
+<div align="center">
+   
+[***Visite o projeto aqui***](https://dsvendas-frontend.netlify.app/)<br />
+</div>
+
 
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
         <footer className="footer mt-auto py-3 bg-dark">
           <div className="container">
             <p className="text-light">App desenvolvido por 
-            <a href="https://github.com/jessicakopps" target="_blank" rel="noreferrer">Jessica Kopps</a></p>
+            <a href="https://github.com/jessicakopps" target="_blank" rel="noreferrer"> Jessica Kopps</a></p>
             <p className="text-light">
               <small><strong>Semana Spring React</strong><br/>
             Evento promovido pela escola DevSuperior: 
